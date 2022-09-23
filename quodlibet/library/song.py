afontenot/quodlibet

commit ea33a61a188da7c345921609d3f207980c5ad305

Update more filters to comprehensions (#4015)

quodlibet/library/song.py
@@ -82,7 +82,7 @@ class SongLibrary(Library[K, V], PicklingMixin):
 
         songs = self.values()
         if text != "":
-            songs = list(filter(Query(text, star).search, songs))
+            songs = [s for s in songs if Query(text, star).search(s)]
         return songs
 
 

quodlibet/browsers/filesystem.py
@@ -1,5 +1,5 @@
 # Copyright 2004-2005 Joe Wreschnig, Michael Urman, Iñigo Serna
-#           2012,2016 Nick Boultbee
+#           2012-2022 Nick Boultbee
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -11,6 +11,7 @@
 # needs them to be there.
 
 import os
+from typing import TypeVar, Iterable, List
 
 from gi.repository import Gtk, Gdk
 from senf import fsn2uri, fsn2bytes, bytes2fsn
@@ -30,6 +31,8 @@ from quodlibet.util.library import get_scan_dirs
 from quodlibet.util.dprint import print_d
 from quodlibet.util.path import normalize_path
 from quodlibet.util import connect_obj
+
+T = TypeVar("T")
 
 
 class FileSystem(Browser, Gtk.HBox):
@@ -69,8 +72,12 @@ class FileSystem(Browser, Gtk.HBox):
 
     @classmethod
     def __remove_because_added(klass, library, songs):
-        songs = list(filter(klass.__library.__contains__, songs))
+        songs = klass._only_known(songs)
         klass.__library.remove(songs)
+
+    @classmethod
+    def _only_known(cls, songs: Iterable[T]) -> List[T]:
+        return [s for s in songs if cls.__library.__contains__(s)]  # type:ignore
 
     def __init__(self, library):
         super().__init__()
@@ -84,8 +91,7 @@ class FileSystem(Browser, Gtk.HBox):
                    ("text/uri-list", 0, self.TARGET_EXT)]
         targets = [Gtk.TargetEntry.new(*t) for t in targets]
 
-        dt.drag_source_set(Gdk.ModifierType.BUTTON1_MASK,
-                           targets, Gdk.DragAction.COPY)
+        dt.drag_source_set(Gdk.ModifierType.BUTTON1_MASK, targets, Gdk.DragAction.COPY)
         dt.connect('drag-data-get', self.__drag_data_get)
 
         sel = dt.get_selection()
@@ -118,7 +124,7 @@ class FileSystem(Browser, Gtk.HBox):
         for songs in self.__find_songs(view.get_selection()):
             pass
         if tid == self.TARGET_QL:
-            cant_add = list(filter(lambda s: not s.can_add, songs))
+            cant_add = [s for s in songs if not s.can_add]
             if cant_add:
                 qltk.ErrorMessage(
                     qltk.get_top_parent(self), _("Unable to copy songs"),
@@ -126,7 +132,7 @@ class FileSystem(Browser, Gtk.HBox):
                       "song lists or the queue.")).run()
                 ctx.drag_abort(etime)
                 return
-            to_add = list(filter(self.__library.__contains__, songs))
+            to_add = self._only_known(songs)
             self.__add_songs(view, to_add)
 
             qltk.selection_set_songs(sel, songs)
@@ -208,11 +214,11 @@ class FileSystem(Browser, Gtk.HBox):
         return menu
 
     def __add_songs(self, item, songs):
-        songs = list(filter(self.__library.__contains__, songs))
+        songs = self._only_known(songs)
         self.__library.librarian.move(songs, self.__library, self.__glibrary)
 
     def __remove_songs(self, songs):
-        songs = list(filter(self.__glibrary.__contains__, songs))
+        songs = [s for s in songs if self.__glibrary.__contains__(s)]
         self.__library.librarian.move(songs, self.__glibrary, self.__library)
 
     def __find_songs(self, selection):
@@ -222,8 +228,9 @@ class FileSystem(Browser, Gtk.HBox):
         to_add = []
         for dir in dirs:
             try:
-                for file in list(filter(formats.filter,
-                                        sorted(os.listdir(dir)))):
+                for file in sorted(os.listdir(dir)):
+                    if not formats.filter(file):
+                        continue
                     raw_path = os.path.join(dir, file)
                     fn = normalize_path(raw_path, canonicalise=True)
                     if fn in self.__glibrary:

quodlibet/browsers/paned/models.py
@@ -1,5 +1,5 @@
 # Copyright 2013 Christoph Reiter
-#           2020 Nick Boultbee
+#        2020-22 Nick Boultbee
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -128,8 +128,7 @@ class PaneModel(ObjectStore):
             return self.__key_cache[song]
         except KeyError:
             # We filter out empty values, so Unknown can be ""
-            self.__key_cache[song] = list(filter(
-                lambda v: v[0], self.config.format(song)))
+            self.__key_cache[song] = [v for v in self.config.format(song) if v[0]]
             return self.__key_cache[song]
 
     def __human_sort_key(self, text, reg=re.compile('<.*?>')):

quodlibet/formats/_audio.py
@@ -290,7 +290,7 @@ class AudioFile(dict, ImageContainer, HasKey):
         """Returns a list of keys that are not internal, i.e. they don't
         have '~' in them."""
 
-        return list(filter(lambda s: s[:1] != "~", self.keys()))
+        return [s for s in self.keys() if s[:1] != "~"]
 
     def prefixkeys(self, prefix):
         """Returns a list of dict keys that either match prefix or start

quodlibet/qltk/information.py
@@ -642,7 +642,7 @@ class Information(Window, PersistentWindowMixin):
     def __check_removed(self, library, songs):
         gone = set(songs)
         old = len(self.__songs)
-        self.__songs = list(filter(lambda s: s not in gone, self.__songs))
+        self.__songs = [s for s in self.__songs if s not in gone]
         if len(self.__songs) != old:
             self.__update(library)
 

quodlibet/qltk/notif.py
@@ -223,8 +223,7 @@ class TaskController:
             self._parent.update()
 
     def finish(self, finished_task):
-        self.active_tasks = list(filter(lambda t: t is not finished_task,
-                                        self.active_tasks))
+        self.active_tasks = [t for t in self.active_tasks if t is not finished_task]
         self.update()
 
 # Oh so deliciously hacky.

quodlibet/qltk/views.py
@@ -208,7 +208,7 @@ class TreeViewHints(Gtk.Window):
         # get the renderer at the mouse position and get the xpos/width
         renderers = col.get_cells()
         pos = sorted(zip(map(col.cell_get_position, renderers), renderers))
-        pos = list(filter(lambda p: p[0][0] < cellx, pos))
+        pos = [p for p in pos if p[0][0] < cellx]
         if not pos:
             self.__undisplay()
             return False

quodlibet/util/importhelper.py
@@ -74,7 +74,7 @@ def get_importables(folder):
                 dirs.remove(d)
         if not first and any((is_init(n) for n in names)):
             yield (basename(root), root,
-                   list(filter(is_ok, [join(root, name) for name in names])))
+                   [d for d in (join(root, name) for name in names) if is_ok(d)])
         else:
             for name in filter(is_ok, names):
                 yield (splitext(name)[0], join(root, name), [join(root, name)])

commit b94be0345b02cbb0204ae9b9d084374c60ff123a

Fix regression of --print-query (#4142)
"quodlibet --print-query=asdf" results in a timeout if the music
collection is large and the main quodlibet instance is started with
the --debug flag because currently Query is unnecessarily instantiated
for every song.  This commit fixes this, which results in a
performance increase even without --debug.

I think commit ea33a61a1 introduced the regression.

quodlibet/library/song.py
@@ -81,7 +81,8 @@ class SongLibrary(Library[K, V], PicklingMixin):
 
         songs = self.values()
         if text != "":
-            songs = [s for s in songs if Query(text, star).search(s)]
+            search = Query(text, star).search
+            songs = [s for s in songs if search(s)]
         return songs
 
 